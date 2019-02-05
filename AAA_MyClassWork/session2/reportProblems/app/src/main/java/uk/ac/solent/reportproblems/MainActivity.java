69d5ed0479c2a923b2f5a65ce024294017e83627
Josh Arrowsmith <3arroj74@solent.ac.uk>
2019-02-05 19:17:07 +0000

    session 4.3

---
+++ AAA_MyClassWork/session2/reportProblems/app/src/main/java/uk/ac/solent/reportproblems/MainActivity.java
@@ -0,0 +1,18 @@
+package uk.ac.solent.reportproblems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
